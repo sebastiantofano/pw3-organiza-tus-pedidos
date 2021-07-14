ccf1d192db348e4312e9ad18a98dad90d833f6ee
fioreColoca <fiorecoloca99@gmail.com>
2021-07-14 12:23:05 -0300

    cambios controller

--- WebAppPedidos/Areas/Administrador/Controllers/ClientesController.cs
+++ WebAppPedidos/Areas/Administrador/Controllers/ClientesController.cs
@@ -66,7 +66,7 @@ namespace WebAppPedidos.Areas.Administrador.Controllers
         public IActionResult AdministrarClientes()
         {
 
-            List<Cliente> listaClientes = _clientesService.ObtenerTodos();
+            List<Cliente> listaClientes = _clientesService.ObtenerTodosOrdenAnalfabetico();
             return View(listaClientes);
         }
 
